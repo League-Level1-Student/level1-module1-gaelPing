5132a176b50b74b579352cc34f18653d620b3e36
student <student@ip-172-31-63-14.us-west-2.compute.internal>
2021-05-31 16:59:17 -0700

    e

---
+++ src/_07_binary_converter/Binary_converter.java
@@ -0,0 +1,21 @@
+package _07_binary_converter;
+
+import java.awt.Dimension;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class Binary_converter {
+	JButton leftButton = new JButton();
+	JButton rightButton = new JButton();
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JTextField text= new JTextField();
+
+
+void button(){
+	panel.add(text);
+}
+}
